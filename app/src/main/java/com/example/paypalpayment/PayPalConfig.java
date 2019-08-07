0a19f92cb10c67483e174d91181b6a093553243c
Hardik <hardik121chitroda@gmail.com>
2019-08-07 11:54:28 +0530

    PaypalPayment

---
+++ app/src/main/java/com/example/paypalpayment/PayPalConfig.java
@@ -0,0 +1,10 @@
+package com.example.paypalpayment;
+
+/**
+ * Created by Belal on 5/1/2016.
+ */
+public class PayPalConfig {
+
+    public static final String PAYPAL_CLIENT_ID = "Adi0P624nw6_X4946L3bMddUCtBnhmBqDsM88PHgGJK-eYB0oYG-1KzmmHcTE8k8ELjcT-xKfccQCmit";
+
+}
